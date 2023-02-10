e3ab23d83413f8a8e1f3706248884281d7da7371
zihaoz96 <zihaoz96@gmail.com>
2023-02-10 11:33:58 +0100

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormulairComponent } from './formulair/formulair.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormulairComponent
   ],
   imports: [
     BrowserModule,
